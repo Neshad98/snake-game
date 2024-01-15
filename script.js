4e1b62b92e1183aea85c5d6fb6bc6c2ba45f3ea1
Neshad <djneshad@gmail.com>
2024-01-15 11:53:06 +0600

    working

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("test");
+//define html elements
+const board = document.getElementById('game-board');
+
+//define game variablels
+let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+
+//draw game map, snake, food
+function draw() {
+  board.innerHTML = '';
+  drawSnake();
+}
+
+//draw snake 
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement('div', 'snake');
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+//create a snake or food cube/div
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+//set the position of snake or food 
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
+
+//testing draw function 
+// draw();
+
+function drawFood() {
+  const foodElement = createGameElement('div', 'food');
+  setPosition(foodElement, food)
+}
